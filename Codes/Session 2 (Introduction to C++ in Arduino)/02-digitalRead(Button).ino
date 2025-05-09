a69e5d69c707a9c0935b6d77f80f9a8f05a0133a
Ahmadreza Heidari <theheidari@gmail.com>
2025-05-09 20:16:42 +0330

    Update 02-digitalRead(Button).ino

--- Codes/Session 2 (Introduction to C++ in Arduino)/02-digitalRead(Button).ino
+++ Codes/Session 2 (Introduction to C++ in Arduino)/02-digitalRead(Button).ino
@@ -1,12 +1,13 @@
 void setup()
 {
-  pinMode(2, OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(4,INPUT);
 }
-
 void loop()
-{
-  digitalWrite(2, HIGH);
-  delay(1000); 
-  digitalWrite(2, LOW);
-  delay(1000); 
+{ 
+  if(digitalRead(4) == HIGH){
+  digitalWrite(2,HIGH);
+  }
+  else{
+    digitalWrite(2,LOW);}
 }
