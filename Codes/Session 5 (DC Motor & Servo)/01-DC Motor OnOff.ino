9df2ada84f1a44b4e350aed1ca5c2141e3463138
Ahmadreza Heidari <theheidari@gmail.com>
2025-05-18 23:13:40 +0330

    Create 01-DC Motor OnOff.ino

---
+++ Codes/Session 5 (DC Motor & Servo)/01-DC Motor OnOff.ino
@@ -0,0 +1,18 @@
+//using IRF520N MOSFET
+void setup()
+{
+  pinMode(3, OUTPUT);
+  pinMode(8,INPUT_PULLUP);
+}
+
+void loop()
+{
+  if(!digitalRead(8)){
+    
+  digitalWrite(3, HIGH);
+  }
+  else{
+     digitalWrite(3, LOW);
+  }
+  
+}
